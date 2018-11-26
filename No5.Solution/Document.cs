7dd2d9c75826f2f24131d9c1d6ff1a2f8eec65a7
Alexandra Gushchenskova <skowa98@gmail.com>
2018-11-26 12:05:59 +0300

    Task5 is added

---
+++ No5.Solution/Document.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using No5.Solution.DocumentParts;
+using No5.Solution.Visitors;
+
+namespace No5.Solution
+{
+    public class Document
+    {
+        private readonly List<DocumentPart> parts;
+
+        public Document(IEnumerable<DocumentPart> parts)
+        {
+            if (parts == null)
+            {
+                throw new ArgumentNullException(nameof(parts));
+            }
+
+            this.parts = new List<DocumentPart>(parts);
+        }
+
+        public string ToHtml() => ToExactType(p => p.ToHtml());
+
+        public string ToPlainText() => ToExactType(p => p.ToPlainText());
+
+        public string ToLaTeX() => ToExactType(p => p.ToLaTeX());
+
+        public string ToVisitorType(DocumentPartVisitor visitor)
+        {
+            return ToExactType(p => visitor.Visit(p));
+        }
+
+        private string ToExactType(Func<DocumentPart, string> convertRule)
+        {
+            string output = string.Empty;
+
+            foreach (DocumentPart part in this.parts)
+            {
+                output += $"{convertRule(part)}\n";
+            }
+
+            return output;
+        }
+    }
+}
